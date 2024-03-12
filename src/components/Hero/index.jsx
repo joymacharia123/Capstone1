7355aa9353e400b4392e813f1c244e061cde6b70
joymacharia <machajoy23@gmail.com>
2024-03-13 02:43:22 +0300

    changes

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,8 +8,14 @@ const Hero = ()=>{
             <div className='edit-12'>Welcome to,</div>
             <div className='edit-22'>Meraki</div>
             <div className='contacts'>
+                <div className='tacts'>
                 <label>Contact us:</label>
                 <div>+254 722 815 082</div>
+                </div>
+                <div className='con'>
+                <label>Email us:</label>
+                <div>merakicuisines@gmail.com</div>
+                </div>
             </div>
         </section>
         </>
